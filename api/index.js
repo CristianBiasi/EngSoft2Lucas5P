9c8e9c084d215ce48c8598341911ed07439e9231
Cristian de Biasi <cristian.biasi20@gmail.com>
2025-07-03 20:39:53 -0300

    correção index abrir sozinha

--- api/index.js
+++ api/index.js
@@ -3,10 +3,18 @@ const imovelRoutes = require("./routes/imoveis");
 const cors = require("cors");
 
 const app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname +'/index.html');
+});
+app.get('/imoveis', function(req, res) {
+  res.sendFile(__dirname +'/index.html');
+});
 app.use(express.json());
 app.use(cors());
 app.use("/imoveis", imovelRoutes);
 
 app.listen(8800, () => {
   console.log("Servidor iniciado com sucesso na porta 8800 🚀");
+  console.log(`Acesse: http://localhost:8800/imoveis`);
 });
